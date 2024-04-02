fc98f86df5d3d1432f508c47c9789fa1e2fd2c8e
LDRushing <rushinglucy@yahoo.com>
2024-04-01 20:58:43 -0500

    final-edits-i-think

--- index.js
+++ index.js
@@ -1,115 +1,106 @@
-const express = require('express')
-const app = express()
-const pg = require('pg')
-const client = new pg.Client(
-  process.env.DATABASE_URL || 'postgres://localhost/employees'
-)
-const port = process.env.PORT || 3000
+// imports here for express and pg
+const express = require('express');
+const app = express();
+const path = require('path');
+const pg = require('pg');
+// static routes here (you only need these for deployment)
+const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/employees')
 
-app.use(express.json())
-app.use(require('morgan')('dev'))
-
+app.use(express.json());
+app.use(require('morgan')('dev'));
+// app routes here
 app.get('/api/employees', async (req, res, next) => {
-  try {
-    const SQL = `
-      SELECT * from employees
-    `
-    const response = await client.query(SQL)
-    res.send(response.rows)
-  } catch (ex) {
-    next(ex)
-  }
-})
-
+    try {
+        const SQL = `
+        SELECT * from employees;
+      `
+        const result = await client.query(SQL)
+        res.send(result.rows)
+    } catch (ex) {
+        next(ex)
+    }
+});
 app.get('/api/departments', async (req, res, next) => {
-  try {
-    const SQL = `
-      SELECT * from departments ORDER BY created_at DESC;
-    `
-    const response = await client.query(SQL)
-    res.send(response.rows)
-  } catch (ex) {
-    next(ex)
-  }
-})
-
+    try {
+        const SQL = `
+        SELECT * from departments;
+      `
+        const result = await client.query(SQL)
+        res.send(result.rows)
+    } catch (ex) {
+        next(ex)
+    }
+});
 app.post('/api/employees', async (req, res, next) => {
-  try {
-    const SQL = `
-      INSERT INTO employees(name, department_id)
-      VALUES($1, $2)
-      RETURNING *
-    `
-    const response = await client.query(SQL, [req.body.name, req.body.department_id])
-    res.send(response.rows[0])
-  } catch (ex) {
-    next(ex)
-  }
-})
-
+    try {
+        const SQL = `INSERT INTO employees(name, department_id)
+        VALUES ($1, (SELECT id FROM departments WHERE name=$2))
+        RETURNING *`
+        const result = await client.query(SQL, [req.body.name, req.body.departmentName])
+        res.send(result.rows[0])
+    } catch (ex) {
+        next(ex)
+    }
+});
 app.delete('/api/employees/:id', async (req, res, next) => {
     try {
-      const SQL = `
-        DELETE from employees
-        WHERE id = $1
+        const SQL = `
+        DELETE FROM employees
+        WHERE id = $1;
       `
-      const response = await client.query(SQL, [req.params.id])
-      res.sendStatus(204)
+        const response = await client.query(SQL, [req.params.id])
+        res.sendStatus(204)
     } catch (ex) {
-      next(ex)
+        next(ex)
     }
-  })
-
+});
 app.put('/api/employees/:id', async (req, res, next) => {
-  try {
-    const SQL = `
-      UPDATE employees
-      SET name=$1, department_id=$2, updated_at= now()
-      WHERE id=$3 RETURNING *
-    `
-    const response = await client.query(SQL, [
-      req.body.name,
-      req.body.department_id,
-      req.params.id
-    ])
-    res.send(response.rows[0])
-  } catch (ex) {
-    next(ex)
-  }
-})
+    try {
+        const SQL = `UPDATE employees
+        SET name = $1, department_id = (SELECT id FROM departments WHERE name=$2), updated_at=now()
+        WHERE id = $3 RETURNING *`
+        const result = await client.query(SQL, [req.body.name, req.body.departmentName, req.params.id])
+        res.send(result.rows[0])
+    } catch (ex) {
+        next(ex)
+    }
+});
 
+// create your init function
 const init = async () => {
-  await client.connect()
-  let SQL = `
-    DROP TABLE IF EXISTS employees;
+    await client.connect();
+    console.log('connected to database')
+    let SQL = `DROP TABLE IF EXISTS employees;
     DROP TABLE IF EXISTS departments;
-    CREATE TABLE departments(
-      id SERIAL PRIMARY KEY,
-      name VARCHAR(100)
-    );
-    CREATE TABLE employees(
-      id SERIAL PRIMARY KEY,
-      created_at TIMESTAMP DEFAULT now(),
-      updated_at TIMESTAMP DEFAULT now(),
-      names VARCHAR(255) NOT NULL,
-      department_id INTEGER REFERENCES departments(id) NOT NULL
-    );
-  `
-  await client.query(SQL)
-  console.log('tables created') //keep this table below as is.
-  SQL = ` 
-    INSERT INTO departments(name) VALUES('Stow');
-    INSERT INTO departments(name) VALUES('Pick');
-    INSERT INTO departments(name) VALUES('Ship Dock');
-    INSERT INTO employees(names, department_id) VALUES('Jenni Smith', (SELECT id FROM departments WHERE name='Pick'));
-    INSERT INTO employees(names, department_id) VALUES('Pepper Thompson', (SELECT id FROM departments WHERE name='Pick'));
-    INSERT INTO employees(names, department_id) VALUES('Jimmy Arthur', (SELECT id FROM departments WHERE name='Stow'));
-    INSERT INTO employees(names, department_id) VALUES('Beth Caspian', (SELECT id FROM departments WHERE name='Stow'));
-    INSERT INTO employees(names, department_id) VALUES('Lauri Underwood', (SELECT id FROM departments WHERE name='Ship Dock'));
-  `
-  await client.query(SQL) //Cascade drops any foreign key that's reference that specific table's ID. 
-  console.log('data seeded')
-  app.listen(port, () => console.log(`listening on port ${port}`))
-}
 
+    CREATE TABLE departments(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL);
+    
+    CREATE TABLE employees(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT now(),
+    updated_at TIMESTAMP DEFAULT now(),
+    department_id INTEGER REFERENCES departments(id) NOT NULL);`
+    await client.query(SQL)
+    console.log('tables created')
+    
+    SQL = ` INSERT INTO departments(name) VALUES('Pick');
+    INSERT INTO departments(name) VALUES('Pack');
+    INSERT INTO departments(name) VALUES('Ship Dock');
+    
+    INSERT INTO employees(name, department_id) VALUES('Jenni Smith', (SELECT id FROM departments WHERE name='Pick'));
+    INSERT INTO employees(name, department_id) VALUES('Pepper Jones', (SELECT id FROM departments WHERE name='Pick'));
+    INSERT INTO employees(name, department_id) VALUES('Terri Bore', (SELECT id FROM departments WHERE name='Pack'));
+    INSERT INTO employees(name, department_id) VALUES('Ina Fae', (SELECT id FROM departments WHERE name='Ship Dock'));
+    INSERT INTO employees(name, department_id) VALUES('Cera Tops', (SELECT id FROM departments WHERE name='Ship Dock'));`;
+    await client.query(SQL);
+    console.log('data seeded');
+
+    const port = process.env.PORT || 3000
+    app.listen(port, () => console.log(`listening on port ${port}`))
+
+}
+// init function invocation
 init()
